1254c219be1898967a2a5e1ecce86022fac8573f
Santiago Echeverri Valencia <santiago.echeverri@pragma.com.co>
2023-05-05 18:43:48 -0500

    limpieza de columnas

--- main.py
+++ main.py
@@ -24,10 +24,36 @@ def read_file(path: str) -> DataFrame:
         logger.error(f'no se pudo leer el path: {path}, {error}')
 
 
+def clean_columns(df: DataFrame, columns: list) -> DataFrame:
+    """Esta función cambia los nombres de las columnas
+
+    Args:
+        df (DataFrame): Dataframe con información
+        columns (list): columnas nuevas
+
+    Returns:
+        DataFrame: dtaa frame con nuevas columnas
+    """
+    try:
+        logger.info(f'renombrando columnas en el df con "-".join({columns})')
+        df.columns = columns
+        return df
+    except Exception as error:
+        logger.error(f'No se pudo actualizar las columnas {error}')
+
+    return df
+
+
+"fipstxt", "state", "County Name", "Persistent Poverty  0=Not persistent poverty 1=Persistent pover", "Persistent Related Child Poverty 0=Not persistent child poverty", "Metro-nonmetro status, 2013 0=Nonmetro 1=Metro"
+
+
 def run():
+    columns = ['fipstxt', 'state', 'county_name',
+               'persistent_poverty', 'persistent_related_child', 'metro_nonmetro_status']
     path = './2015_persistent_povert.csv'
     df = read_file(path)
-    print(df.columns)
+    df = clean_columns(df, columns)
+    print(df.dtypes)
 
 
 run()
